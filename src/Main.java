5a53d80d54825bd0f031988e903661b5894abe86
Matheus Muriel <matheus.muriel@outlook.com>
2021-04-27 13:34:48 -0300

    Regex validation input

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,8 @@ import Utils.Cores;
 import Utils.StatusJogo;
 
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Main {
     private static Scanner scanner = new Scanner(System.in);
@@ -28,7 +30,24 @@ public class Main {
             System.out.println("Digite sua jogada no formato: \"Origem,Destino\" (Ex. D2,D4)");
             System.out.print("Jogada: ");
             String input = scanner.nextLine();
+            while (!verificarInput(input)) {
+                System.out.println("Entrada invalida! Por favor digite novamente");
+                System.out.print("Jogada: ");
+                input = scanner.nextLine();
+            }
+            System.out.println("Entrada Valida");
         }
+    }
+
+    private static boolean verificarInput(String input) {
+        String[] posicoes = input.split(",");
+        if (posicoes.length < 2) {
+            return false;
+        }
+        Pattern pattern = Pattern.compile("^[abcdefghABCDEFGH]{1}[1-8]{1}$");
+        Matcher matcher1 = pattern.matcher(posicoes[0]);
+        Matcher matcher2 = pattern.matcher(posicoes[1]);
+        return matcher1.find() && matcher2.find();
     }
 
 }
