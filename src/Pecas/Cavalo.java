88097312e18ba747f875f93207e00ee0571d0880
Matheus Muriel <matheus.muriel@outlook.com>
2021-04-29 16:07:15 -0300

    Movimento Cavalo

--- src/Pecas/Cavalo.java
+++ src/Pecas/Cavalo.java
@@ -19,11 +19,230 @@ public class Cavalo extends Peca {
 
     @Override
     public boolean isMovimentoValido(Posicao posicao, Tabuleiro tabuleiro) {
-        return false;
+        Posicao posicaoAtual = super.getPosicaoAtual();
+
+        int linha = posicaoAtual.getLinha();
+        int indexColuna = tabuleiro.getColunas().indexOf(super.getPosicaoAtual().getColuna());
+
+        ArrayList<Posicao> movimentosValidos = new ArrayList<>();
+
+        // M1
+        linha += 1;
+        indexColuna -= 2;
+        if(indexColuna < tabuleiro.getColunas().size() && indexColuna >= 0) {
+            Posicao p1 = new Posicao(tabuleiro.getColunas().get(indexColuna), linha);
+            if(tabuleiro.isMovimentoValido(p1)) {
+                movimentosValidos.add(p1);
+            }
+        }
+        linha = posicaoAtual.getLinha();
+        indexColuna = tabuleiro.getColunas().indexOf(super.getPosicaoAtual().getColuna());
+
+
+        // M2
+        linha += 2;
+        indexColuna -= 1;
+        if(indexColuna < tabuleiro.getColunas().size() && indexColuna >= 0) {
+            Posicao p1 = new Posicao(tabuleiro.getColunas().get(indexColuna), linha);
+            if(tabuleiro.isMovimentoValido(p1)) {
+                movimentosValidos.add(p1);
+            }
+        }
+        linha = posicaoAtual.getLinha();
+        indexColuna = tabuleiro.getColunas().indexOf(super.getPosicaoAtual().getColuna());
+
+
+        // M3
+        linha += 2;
+        indexColuna += 1;
+        if(indexColuna < tabuleiro.getColunas().size() && indexColuna >= 0) {
+            Posicao p1 = new Posicao(tabuleiro.getColunas().get(indexColuna), linha);
+            if(tabuleiro.isMovimentoValido(p1)) {
+                movimentosValidos.add(p1);
+            }
+        }
+        linha = posicaoAtual.getLinha();
+        indexColuna = tabuleiro.getColunas().indexOf(super.getPosicaoAtual().getColuna());
+
+
+        // M4
+        linha += 1;
+        indexColuna += 2;
+        if(indexColuna < tabuleiro.getColunas().size() && indexColuna >= 0) {
+            Posicao p1 = new Posicao(tabuleiro.getColunas().get(indexColuna), linha);
+            if(tabuleiro.isMovimentoValido(p1)) {
+                movimentosValidos.add(p1);
+            }
+        }
+        linha = posicaoAtual.getLinha();
+        indexColuna = tabuleiro.getColunas().indexOf(super.getPosicaoAtual().getColuna());
+        
+        // M5
+        linha -= 1;
+        indexColuna += 2;
+        if(indexColuna < tabuleiro.getColunas().size() && indexColuna >= 0) {
+            Posicao p1 = new Posicao(tabuleiro.getColunas().get(indexColuna), linha);
+            if(tabuleiro.isMovimentoValido(p1)) {
+                movimentosValidos.add(p1);
+            }
+        }
+        linha = posicaoAtual.getLinha();
+        indexColuna = tabuleiro.getColunas().indexOf(super.getPosicaoAtual().getColuna());
+
+
+        // M6
+        linha -= 2;
+        indexColuna += 1;
+        if(indexColuna < tabuleiro.getColunas().size() && indexColuna >= 0) {
+            Posicao p1 = new Posicao(tabuleiro.getColunas().get(indexColuna), linha);
+            if(tabuleiro.isMovimentoValido(p1)) {
+                movimentosValidos.add(p1);
+            }
+        }
+        linha = posicaoAtual.getLinha();
+        indexColuna = tabuleiro.getColunas().indexOf(super.getPosicaoAtual().getColuna());
+
+
+        // M7
+        linha -= 2;
+        indexColuna -= 1;
+        if(indexColuna < tabuleiro.getColunas().size() && indexColuna >= 0) {
+            Posicao p1 = new Posicao(tabuleiro.getColunas().get(indexColuna), linha);
+            if(tabuleiro.isMovimentoValido(p1)) {
+                movimentosValidos.add(p1);
+            }
+        }
+        linha = posicaoAtual.getLinha();
+        indexColuna = tabuleiro.getColunas().indexOf(super.getPosicaoAtual().getColuna());
+
+
+        // M8
+        linha -= 1;
+        indexColuna -= 2;
+        if(indexColuna < tabuleiro.getColunas().size() && indexColuna >= 0) {
+            Posicao p1 = new Posicao(tabuleiro.getColunas().get(indexColuna), linha);
+            if(tabuleiro.isMovimentoValido(p1)) {
+                movimentosValidos.add(p1);
+            }
+        }
+
+        boolean jogadaValida = movimentosValidos.stream().anyMatch(p -> p.equalsTo(posicao));
+
+        return jogadaValida || capturaEm(posicao, tabuleiro);
     }
 
     @Override
     public boolean capturaEm(Posicao posicao, Tabuleiro tabuleiro) {
-        return false;
+        Posicao posicaoAtual = super.getPosicaoAtual();
+
+        int linha = posicaoAtual.getLinha();
+        int indexColuna = tabuleiro.getColunas().indexOf(super.getPosicaoAtual().getColuna());
+
+        ArrayList<Posicao> movimentosValidos = new ArrayList<>();
+
+        // M1
+        linha += 1;
+        indexColuna -= 2;
+        if(indexColuna < tabuleiro.getColunas().size() && indexColuna >= 0) {
+            Posicao p1 = new Posicao(tabuleiro.getColunas().get(indexColuna), linha);
+            if(tabuleiro.isMovimentoValido(p1)) {
+                movimentosValidos.add(p1);
+            }
+        }
+        linha = posicaoAtual.getLinha();
+        indexColuna = tabuleiro.getColunas().indexOf(super.getPosicaoAtual().getColuna());
+
+
+        // M2
+        linha += 2;
+        indexColuna -= 1;
+        if(indexColuna < tabuleiro.getColunas().size() && indexColuna >= 0) {
+            Posicao p1 = new Posicao(tabuleiro.getColunas().get(indexColuna), linha);
+            if(tabuleiro.isMovimentoValido(p1)) {
+                movimentosValidos.add(p1);
+            }
+        }
+        linha = posicaoAtual.getLinha();
+        indexColuna = tabuleiro.getColunas().indexOf(super.getPosicaoAtual().getColuna());
+
+
+        // M3
+        linha += 2;
+        indexColuna += 1;
+        if(indexColuna < tabuleiro.getColunas().size() && indexColuna >= 0) {
+            Posicao p1 = new Posicao(tabuleiro.getColunas().get(indexColuna), linha);
+            if(tabuleiro.isMovimentoValido(p1)) {
+                movimentosValidos.add(p1);
+            }
+        }
+        linha = posicaoAtual.getLinha();
+        indexColuna = tabuleiro.getColunas().indexOf(super.getPosicaoAtual().getColuna());
+
+
+        // M4
+        linha += 1;
+        indexColuna += 2;
+        if(indexColuna < tabuleiro.getColunas().size() && indexColuna >= 0) {
+            Posicao p1 = new Posicao(tabuleiro.getColunas().get(indexColuna), linha);
+            if(tabuleiro.isMovimentoValido(p1)) {
+                movimentosValidos.add(p1);
+            }
+        }
+        linha = posicaoAtual.getLinha();
+        indexColuna = tabuleiro.getColunas().indexOf(super.getPosicaoAtual().getColuna());
+        
+        // M5
+        linha -= 1;
+        indexColuna += 2;
+        if(indexColuna < tabuleiro.getColunas().size() && indexColuna >= 0) {
+            Posicao p1 = new Posicao(tabuleiro.getColunas().get(indexColuna), linha);
+            if(tabuleiro.isMovimentoValido(p1)) {
+                movimentosValidos.add(p1);
+            }
+        }
+        linha = posicaoAtual.getLinha();
+        indexColuna = tabuleiro.getColunas().indexOf(super.getPosicaoAtual().getColuna());
+
+
+        // M6
+        linha -= 2;
+        indexColuna += 1;
+        if(indexColuna < tabuleiro.getColunas().size() && indexColuna >= 0) {
+            Posicao p1 = new Posicao(tabuleiro.getColunas().get(indexColuna), linha);
+            if(tabuleiro.isMovimentoValido(p1)) {
+                movimentosValidos.add(p1);
+            }
+        }
+        linha = posicaoAtual.getLinha();
+        indexColuna = tabuleiro.getColunas().indexOf(super.getPosicaoAtual().getColuna());
+
+
+        // M7
+        linha -= 2;
+        indexColuna -= 1;
+        if(indexColuna < tabuleiro.getColunas().size() && indexColuna >= 0) {
+            Posicao p1 = new Posicao(tabuleiro.getColunas().get(indexColuna), linha);
+            if(tabuleiro.isMovimentoValido(p1)) {
+                movimentosValidos.add(p1);
+            }
+        }
+        linha = posicaoAtual.getLinha();
+        indexColuna = tabuleiro.getColunas().indexOf(super.getPosicaoAtual().getColuna());
+
+
+        // M8
+        linha -= 1;
+        indexColuna -= 2;
+        if(indexColuna < tabuleiro.getColunas().size() && indexColuna >= 0) {
+            Posicao p1 = new Posicao(tabuleiro.getColunas().get(indexColuna), linha);
+            if(tabuleiro.isMovimentoValido(p1)) {
+                movimentosValidos.add(p1);
+            }
+        }
+
+        return movimentosValidos.stream().anyMatch(p -> {
+            Optional<Peca> opPeca = tabuleiro.existePecaNaPosicao(p.getColuna(), p.getLinha());
+            return opPeca.filter(peca -> p.equalsTo(posicao) && !peca.getCor().equals(super.getCor())).isPresent();
+        });
     }
 }
